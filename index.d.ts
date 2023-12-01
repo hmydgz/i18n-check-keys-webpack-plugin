8c4aa06f96af0f8e1c5455704d7e25988e6bc4b9
hmydgz <920136080@qq.com>
2023-12-01 17:57:34 +0800

    ✨ feat: 调整类型声明

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
 import { checkI18nKeys } from 'i18n-check-keys'
 
-export default class I18nCheckKeysPlugin {
+declare class I18nCheckKeysPlugin {
   constructor(options: Parameters<typeof checkI18nKeys>[0])
 }
+
+export = I18nCheckKeysPlugin
